5887c519b22b378b6bd2f0866fb1ed639dae197c
Lukasz Barszczewski <lukasz.barszczewski@yahoo.com>
2025-02-12 20:10:35 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { WhyMeSectionComponent } from './components/why-me-section/why-me-section.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroSectionComponent, MenuBarComponent, WhyMeSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/why-me-section/why-me-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-why-me-section',
+  imports: [],
+  templateUrl: './why-me-section.component.html',
+  styleUrl: './why-me-section.component.scss'
+})
+export class WhyMeSectionComponent {
+
+}
