8a9aa233f348c01747073d608cb9c8746c1837e6
Lukasz Barszczewski <lukasz.barszczewski@yahoo.com>
2025-02-27 17:26:37 +0100

    extended the contact-me section, added a contact formular

--- src/app/components/contact-me/contact-me.component.ts
+++ src/app/components/contact-me/contact-me.component.ts
@@ -2,10 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact-me',
-  imports: [],
+  standalone: true,
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
+  isChecked = false;
 
+  toggleCheckbox() {
+    this.isChecked = !this.isChecked;
+  }
+
+  get checkboxImage() {
+    return this.isChecked 
+      ? 'assets/img/checkbox-checked.png' 
+      : 'assets/img/checkbox-unchecked.png';
+  }
 }
