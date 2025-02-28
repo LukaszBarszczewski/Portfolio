39a8724c442a52bd793bf853adadde52d8d36e3d
Lukasz Barszczewski <lukasz.barszczewski@yahoo.com>
2025-02-28 15:15:00 +0100

    added footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,19 @@ import { ProjectsComponent } from './components/projects/projects.component';
 import { ReferencesComponent } from './components/references/references.component';
 import { ContactMeComponent } from './components/contact-me/contact-me.component';
 import { MySkillsComponent } from './components/my-skills/my-skills.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroSectionComponent, MenuBarComponent, WhyMeSectionComponent, ProjectsComponent, ReferencesComponent, ContactMeComponent, MySkillsComponent],
+  imports: [RouterOutlet,
+    HeroSectionComponent,
+    MenuBarComponent,
+    WhyMeSectionComponent,
+    ProjectsComponent,
+    ReferencesComponent,
+    ContactMeComponent,
+    MySkillsComponent,
+    FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
