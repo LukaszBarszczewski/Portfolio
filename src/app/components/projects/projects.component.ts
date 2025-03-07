9fbf52185db30e0ff93a0a8e209fbb9e8f9a07dc
Lukasz Barszczewski <lukasz.barszczewski@yahoo.com>
2025-03-07 10:24:39 +0100

    added funtionality and style to projects section

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -12,32 +12,38 @@ export class ProjectsComponent {
     {
       title: 'Join',
       details: [
-        'About the project',
-        'How I have organised my work process',
-        'What I have learnt'
+        'This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization.',
+        'How do you keep your code clean and maintainable? Have you broken the project down into reusable modules or components? Focus on documentation, naming files, variables, classes and testing.',
+        'How many people were in the team and what was your role? Describe your tasks in 1-2 sentences, for example: login form, dashboard or chat functionality.What technologies did you use?It is nice to mention good teamwork and cooperation.'
       ],
-      image: 'assets/img/?',
+      image: 'assets/img/dabubble.jpeg',
       link: '#'
     },
     {
       title: 'World of World.class',
       details: [
-        'About the project',
-        'How I have organised my work process',
-        'What I have learnt'
+        'This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization.',
+        'How do you keep your code clean and maintainable? Have you broken the project down into reusable modules or components? Focus on documentation, naming files, variables, classes and testing.',
+        'How many people were in the team and what was your role? Describe your tasks in 1-2 sentences, for example: login form, dashboard or chat functionality.What technologies did you use?It is nice to mention good teamwork and cooperation.'
       ],
-      image: 'assets/img/?',
+      image: 'assets/img/dabubble.jpeg',
       link: '#'
     },
     {
       title: 'Pokedex',
       details: [
-        'About the project',
-        'How I have organised my work process',
-        'What I have learnt'
+        'This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization.',
+        'How do you keep your code clean and maintainable? Have you broken the project down into reusable modules or components? Focus on documentation, naming files, variables, classes and testing.',
+        'How many people were in the team and what was your role? Describe your tasks in 1-2 sentences, for example: login form, dashboard or chat functionality.What technologies did you use?It is nice to mention good teamwork and cooperation.'
       ],
-      image: 'assets/img/?',
+      image: 'assets/img/dabubble.jpeg',
       link: '#'
     }
   ];
+
+  activeProjectIndex = 0;
+
+  selectProject(index: number) {
+    this.activeProjectIndex = index;
+  }
 }
