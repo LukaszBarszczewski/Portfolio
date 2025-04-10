e43ad756de78718807ee32a83cf3108ac60ec535
Lukasz Barszczewski <lukasz.barszczewski@yahoo.com>
2025-04-10 17:49:34 +0200

    upgraded the form validation of the contact form

--- src/app/components/contact-me/contact-me.component.ts
+++ src/app/components/contact-me/contact-me.component.ts
@@ -1,15 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
   isDisabled: boolean = true;
 
-  updateButtonState(event: Event) {
-    this.isDisabled = !(event.target as HTMLInputElement).checked;
+  updateButtonState() {
+    const checkbox = (document.getElementById('privacy') as HTMLInputElement)?.checked;
+    const { name, email, message } = this.contactData;
+    this.isDisabled = !(checkbox && name && email && message);
+  }
+
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData);
+    }
+    
   }
 }
